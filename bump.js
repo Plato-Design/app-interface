6b0a1441fbdc932e61571c72c272d06e088a009d
Matthew Trost <mrtrost@gmail.com>
2022-08-03 11:46:19 -0400

    script to bump

---
+++ bump.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const cp = require('child_process')
+const semver = require('semver')
+
+cp.execSync('yarn compile', {stdio: 'inherit'})
+
+const pkg = require('./package.json')
+const version = semver.inc(pkg.version, 'patch')
+console.info(`Updated ${pkg.version} to ${version}`)
+pkg.version = version
+fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2))
+
+cp.execSync('git add .', {stdio: 'inherit'})
+cp.execSync('git commit -m "bump"', {stdio: 'inherit'})
+cp.execSync('git push origin master', {stdio: 'inherit'})
+
+cp.execSync(`yarn publish --non-interactive --new-version ${version}`, {stdio: 'inherit'})
