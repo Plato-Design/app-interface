f5b3e65011fca4220644c27ccddc1adbcc0601c3
Matthew Trost <mrtrost@gmail.com>
2022-08-04 17:29:00 -0400

    bump

--- bump.js
+++ bump.js
@@ -12,6 +12,8 @@ fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2))
 
 cp.execSync('git add .', {stdio: 'inherit'})
 cp.execSync('git commit -m "bump"', {stdio: 'inherit'})
+cp.execSync(`git tag v${version}`, {stdio: 'inherit'})
 cp.execSync('git push origin master', {stdio: 'inherit'})
+cp.execSync('git push origin --tags', {stdio: 'inherit'})
 
 cp.execSync(`yarn publish --non-interactive --new-version ${version}`, {stdio: 'inherit'})
