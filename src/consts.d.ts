fbdf931fe9bec5344bf6545ecd2a361bb3d399a4
Matthew Trost <mrtrost@gmail.com>
2022-08-12 10:57:41 -0400

    bump

--- src/consts.d.ts
+++ src/consts.d.ts
@@ -1,1 +1,2 @@
 export declare const WALLET_ADDRESS_SYMBOL = "$WALLET_ADDRESS";
+export declare const MINT_QUANTITY_SYMBOL = "$MINT_QUANTITY";
--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,5 @@
 "use strict";
 exports.__esModule = true;
-exports.WALLET_ADDRESS_SYMBOL = void 0;
+exports.MINT_QUANTITY_SYMBOL = exports.WALLET_ADDRESS_SYMBOL = void 0;
 exports.WALLET_ADDRESS_SYMBOL = "$WALLET_ADDRESS";
+exports.MINT_QUANTITY_SYMBOL = "$MINT_QUANTITY";
